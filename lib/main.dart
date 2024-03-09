bc2d65041104b6aa876b6bd6f9065065822a57b1
unknown <syifalialia26@gmail.com>
2024-03-09 11:52:42 +0700

    added 20 Media Query

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+//import 'package:flutter/services.dart';
 // import 'package:section_4/16%20Routes/routes_gallery_page.dart';
 // import 'package:section_4/16%20Routes/routes_home_page.dart.dart';
 // import 'package:section_4/16%20Routes/routes_photo.dart';
-import 'package:section_4/19%20Theme/theme.dart';
+import 'package:section_4/20%20Media%20Query/media_query.dart';
 
 void main() {
+  //supaya gabisa dipotrait dan landscape
+  // WidgetsFlutterBinding();
+  // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitDown]);
+  //portraitUp, yang berarti aplikasi hanya akan mendukung orientasi potret dan akan tetap berada dalam mode potret meskipun perangkat fisiknya mungkin dapat dirotasi ke orientasi lainnya.
+
   runApp(MyApp());
 }
 
@@ -15,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyTheme(),
+      home: med_query(),
 
       //MENDAFTARKAN SECARA MANUAL:
       // initialRoute: '/homepage', //halaman pertama yang dibuka
@@ -35,40 +41,40 @@ class MyApp extends StatelessWidget {
       // },
 
 //--------------------------------------------------------------------------------------//
-      theme: ThemeData(
-          // brightness: Brightness.dark, //defaultnya white//tema layar
-          //visualDensity: VisualDensity.adaptivePlatformDensity, //default
-          visualDensity: VisualDensity(
-              horizontal: 4, vertical: 4), //untuk ngituin posisi default fitur
+      // theme: ThemeData(
+      //     // brightness: Brightness.dark, //defaultnya white//tema layar
+      //     //visualDensity: VisualDensity.adaptivePlatformDensity, //default
+      //     visualDensity: VisualDensity(
+      //         horizontal: 4, vertical: 4), //untuk ngituin posisi default fitur
 
-          // primarySwatch: MaterialColor(
-          //   0xffe55f48, // 0% comes in here, this will be color picked if no shade is selected when defining a Color property which doesn’t require a swatch.
-          //   const <int, Color>{
-          //     50: const Color(0xffce5641), //10%
-          //     100: const Color(0xffb74c3a), //20%
-          //     200: const Color(0xffa04332), //30%
-          //     300: const Color(0xff89392b), //40%
-          //     400: const Color(0xff733024), //50%
-          //     500: const Color(0xff5c261d), //60%
-          //     600: const Color(0xff451c16), //70%
-          //     700: const Color(0xff2e130e), //80%
-          //     800: const Color(0xff170907), //90%
-          //     900: const Color(0xff000000), //100%
-          //   },
-          appBarTheme: AppBarTheme(
-              color: Colors.purpleAccent,
-              titleTextStyle: TextStyle(
-                  fontFamily: "Anta", color: Colors.limeAccent, fontSize: 30)),
-          primarySwatch: Colors
-              .amber, //appbar sama buttonnya sama floatiingnya warnanya sama //LAPISAN 1 INTI
-          primaryColor:
-              Colors.deepOrange, //warna appbarnya aja yang kuning //LAPISAN 2
-          //accentColor: Colors.green,
-          textTheme: TextTheme(
-              bodyMedium: TextStyle(
-            fontFamily: "Anta",
-            color: Colors.amber,
-          ))),
+      //     // primarySwatch: MaterialColor(
+      //     //   0xffe55f48, // 0% comes in here, this will be color picked if no shade is selected when defining a Color property which doesn’t require a swatch.
+      //     //   const <int, Color>{
+      //     //     50: const Color(0xffce5641), //10%
+      //     //     100: const Color(0xffb74c3a), //20%
+      //     //     200: const Color(0xffa04332), //30%
+      //     //     300: const Color(0xff89392b), //40%
+      //     //     400: const Color(0xff733024), //50%
+      //     //     500: const Color(0xff5c261d), //60%
+      //     //     600: const Color(0xff451c16), //70%
+      //     //     700: const Color(0xff2e130e), //80%
+      //     //     800: const Color(0xff170907), //90%
+      //     //     900: const Color(0xff000000), //100%
+      //     //   },
+      //     appBarTheme: AppBarTheme(
+      //         color: Colors.purpleAccent,
+      //         titleTextStyle: TextStyle(
+      //             fontFamily: "Anta", color: Colors.limeAccent, fontSize: 30)),
+      //     primarySwatch: Colors
+      //         .amber, //appbar sama buttonnya sama floatiingnya warnanya sama //LAPISAN 1 INTI
+      //     primaryColor:
+      //         Colors.deepOrange, //warna appbarnya aja yang kuning //LAPISAN 2
+      //     //accentColor: Colors.green,
+      //     textTheme: TextTheme(
+      //         bodyMedium: TextStyle(
+      //       fontFamily: "Anta",
+      //       color: Colors.amber,
+      //     ))),
     );
   }
 }
